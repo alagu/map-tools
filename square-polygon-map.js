e2093a487efdbf02ad54081689b35c5dfabfc74a
Alagu <alagu@alagu.net>
2014-11-23 21:29:23 +0530

    Set proper zoom level

--- square-polygon-map.js
+++ square-polygon-map.js
@@ -5,6 +5,34 @@ function getParameterByName(name) {
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+function getBoundsZoomLevel(bounds, mapDim) {
+    var WORLD_DIM = { height: 256, width: 256 };
+    var ZOOM_MAX = 21;
+
+    function latRad(lat) {
+        var sin = Math.sin(lat * Math.PI / 180);
+        var radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
+        return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
+    }
+
+    function zoom(mapPx, worldPx, fraction) {
+        return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
+    }
+
+    var ne = bounds.getNorthEast();
+    var sw = bounds.getSouthWest();
+
+    var latFraction = (latRad(ne.lat()) - latRad(sw.lat())) / Math.PI;
+
+    var lngDiff = ne.lng() - sw.lng();
+    var lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
+
+    var latZoom = zoom(mapDim.height, WORLD_DIM.height, latFraction);
+    var lngZoom = zoom(mapDim.width, WORLD_DIM.width, lngFraction);
+
+    return Math.min(latZoom, lngZoom, ZOOM_MAX);
+}
+
 function initialize() {
 
   var top = getParameterByName('top') || 12.980802;
@@ -12,16 +40,7 @@ function initialize() {
   var left = getParameterByName('left') || 77.568855;
   var right = getParameterByName('right') || 77.574424;
 
-  var mapOptions = {
-    zoom: 10,
-    center: new google.maps.LatLng(top, right)
-  };
-
-  var bermudaTriangle;
-
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-
+  
   // Define the LatLng coordinates for the polygon's path.
   var triangleCoords = [
     new google.maps.LatLng(top, right),
@@ -30,8 +49,27 @@ function initialize() {
     new google.maps.LatLng(bottom, right)
   ];
 
+
+  var latlngbounds = new google.maps.LatLngBounds();
+  for (var i = 0; i < triangleCoords.length; i++) {
+    latlngbounds.extend(triangleCoords[i]);
+  }
+
+  var mapDiv = document.getElementById('map-canvas');
+  var mapDim = { height: mapDiv.clientHeight, width: mapDiv.clientWidth };
+
+  var mapOptions = {
+    zoom: getBoundsZoomLevel(latlngbounds, mapDim),
+    center: latlngbounds.getCenter()
+  };
+
+  var squarePolygon;
+
+
+  var map = new google.maps.Map(mapDiv, mapOptions);
+
   // Construct the polygon.
-  bermudaTriangle = new google.maps.Polygon({
+  squarePolygon = new google.maps.Polygon({
     paths: triangleCoords,
     strokeColor: '#0000FF',
     strokeOpacity: 0.7,
@@ -40,7 +78,7 @@ function initialize() {
     fillOpacity: 0.1
   });
 
-  bermudaTriangle.setMap(map);
+  squarePolygon.setMap(map);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
